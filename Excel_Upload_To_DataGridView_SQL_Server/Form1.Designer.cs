cc4d4fe702111c11182a104fc909a7a8be644e3e
rhrashal <rhrashal@gmail.com>
2022-06-03 14:52:40 +0600

    initial upload

---
+++ Excel_Upload_To_DataGridView_SQL_Server/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace Excel_Upload_To_DataGridView_SQL_Server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt_Url = new System.Windows.Forms.TextBox();
+            this.btn_format = new System.Windows.Forms.Button();
+            this.btn_ExcelUpload = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btn_save = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txt_Url
+            // 
+            this.txt_Url.Location = new System.Drawing.Point(25, 13);
+            this.txt_Url.Name = "txt_Url";
+            this.txt_Url.Size = new System.Drawing.Size(756, 22);
+            this.txt_Url.TabIndex = 0;
+            this.txt_Url.Click += new System.EventHandler(this.txt_Url_Click);
+            // 
+            // btn_format
+            // 
+            this.btn_format.Location = new System.Drawing.Point(787, 12);
+            this.btn_format.Name = "btn_format";
+            this.btn_format.Size = new System.Drawing.Size(125, 23);
+            this.btn_format.TabIndex = 1;
+            this.btn_format.Text = "Format Excel";
+            this.btn_format.UseVisualStyleBackColor = true;
+            this.btn_format.Click += new System.EventHandler(this.btn_format_Click);
+            // 
+            // btn_ExcelUpload
+            // 
+            this.btn_ExcelUpload.Location = new System.Drawing.Point(918, 13);
+            this.btn_ExcelUpload.Name = "btn_ExcelUpload";
+            this.btn_ExcelUpload.Size = new System.Drawing.Size(127, 23);
+            this.btn_ExcelUpload.TabIndex = 2;
+            this.btn_ExcelUpload.Text = "Upload Excel";
+            this.btn_ExcelUpload.UseVisualStyleBackColor = true;
+            this.btn_ExcelUpload.Click += new System.EventHandler(this.btn_ExcelUpload_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(25, 64);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1020, 432);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // btn_save
+            // 
+            this.btn_save.Location = new System.Drawing.Point(441, 502);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(125, 40);
+            this.btn_save.TabIndex = 4;
+            this.btn_save.Text = "Save";
+            this.btn_save.UseVisualStyleBackColor = true;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.btn_save);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_ExcelUpload);
+            this.Controls.Add(this.btn_format);
+            this.Controls.Add(this.txt_Url);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Excel Data Upload";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt_Url;
+        private System.Windows.Forms.Button btn_format;
+        private System.Windows.Forms.Button btn_ExcelUpload;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btn_save;
+    }
+}
+
---
+++ Excel_Upload_To_DataGridView_SQL_Server/Form1.cs
@@ -0,0 +1,202 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace Excel_Upload_To_DataGridView_SQL_Server
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+      
+
+       
+
+        private void txt_Url_Click(object sender, EventArgs e)
+        {
+            //https://www.c-sharpcorner.com/UploadFile/bd6c67/how-to-create-excel-file-using-C-Sharp/
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.InitialDirectory = @"C:\";
+            saveFileDialog1.Title = "Save Excel Files";
+
+            saveFileDialog1.DefaultExt = "xlsx";
+            saveFileDialog1.Filter = "Excel Files (*.xlsx)|*.xlsx|All files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 1;
+            saveFileDialog1.RestoreDirectory = true;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txt_Url.Text = saveFileDialog1.FileName;
+
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        private void btn_format_Click(object sender, EventArgs e)
+        {
+            //https://www.c-sharpcorner.com/UploadFile/bd6c67/how-to-create-excel-file-using-C-Sharp/
+            Microsoft.Office.Interop.Excel.Application excel;
+            Microsoft.Office.Interop.Excel.Workbook worKbooK;
+            Microsoft.Office.Interop.Excel.Worksheet worKsheeT;
+            //Microsoft.Office.Interop.Excel.Range celLrangE;
+
+            try
+            {
+                excel = new Microsoft.Office.Interop.Excel.Application();
+                excel.Visible = false;
+                excel.DisplayAlerts = false;
+                worKbooK = excel.Workbooks.Add(Type.Missing);
+
+
+                worKsheeT = (Microsoft.Office.Interop.Excel.Worksheet)worKbooK.ActiveSheet;
+                worKsheeT.Name = "EmployeeSheet";
+
+                //worKsheeT.Range[worKsheeT.Cells[1, 1], worKsheeT.Cells[1, 8]].Merge();
+                worKsheeT.Cells[1, 1] = "SL_NO";
+                worKsheeT.Cells[1, 2] = "EmployeeName";
+                worKsheeT.Cells[1, 3] = "Phone";
+                worKsheeT.Cells[1, 4] = "Email";
+                worKsheeT.Cells[1, 5] = "Address";
+                worKsheeT.Cells.Font.Size = 15;
+
+                //int rowcount = 1;              
+                //celLrangE = worKsheeT.Range[worKsheeT.Cells[1, 1], worKsheeT.Cells[rowcount, ExportToExcel().Columns.Count]];
+                //celLrangE.EntireColumn.AutoFit();
+                //Microsoft.Office.Interop.Excel.Borders border = celLrangE.Borders;
+                //border.LineStyle = Microsoft.Office.Interop.Excel.XlLineStyle.xlContinuous;
+                //border.Weight = 2d;
+
+
+                //celLrangE = worKsheeT.Range[worKsheeT.Cells[1, 1], worKsheeT.Cells[2, ExportToExcel().Columns.Count]];
+
+
+                worKbooK.SaveAs(txt_Url.Text); ;
+                worKbooK.Close();
+                excel.Quit();
+                MessageBox.Show("Successfully Create Excel File");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+            finally
+            {
+                worKsheeT = null;
+                worKbooK = null;
+            }
+        }
+
+        private void btn_ExcelUpload_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog(); //open dialog to choose file
+            if (file.ShowDialog() == DialogResult.OK) //if there is a file chosen by the user
+            {
+                string fileExt = Path.GetExtension(file.FileName); //get the file extension
+                if (fileExt.CompareTo(".xls") == 0 || fileExt.CompareTo(".xlsx") == 0)
+                {
+                    try
+                    {
+                        DataTable dtExcel = ReadExcel(file.FileName); //read excel file
+                        dataGridView1.Visible = true;
+                        dataGridView1.DataSource = dtExcel;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message.ToString());
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please choose .xls or .xlsx file only.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error); //custom messageBox to show error
+                }
+            }
+        }
+
+        private DataTable GetDataTable(string sql, string connectionString)
+        {
+            using (OleDbConnection conn = new OleDbConnection(connectionString))
+            {
+                conn.Open();
+                System.Data.OleDb.OleDbDataAdapter adapter = new System.Data.OleDb.OleDbDataAdapter(sql, conn);
+                System.Data.DataSet ds = new System.Data.DataSet();
+                adapter.Fill(ds);
+                return ds.Tables[0];
+            }
+        }
+        private DataTable ReadExcel(string fileName)
+        {
+            // if error -> The 'Microsoft.ACE.OLEDB.12.0' provider is not registered on the local machine
+            // in this menu: project -> yourproject properties... -> Build : uncheck "prefer 32-Bit"
+            string connString = string.Format("Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties='Excel 12.0;HDR=yes'", fileName);
+            DataTable dt = GetDataTable("SELECT * from [EmployeeSheet$]", connString);
+            return dt;
+        }
+
+        private void btn_save_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string connectionString = ConfigurationManager.ConnectionStrings["DBConn"].ConnectionString;
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+
+                    conn.Open();
+                    int count = 0;
+                    foreach (DataGridViewRow row in dataGridView1.Rows)
+                    {
+
+                        if (!row.IsNewRow)
+                        {
+                            using (SqlCommand cmd = new SqlCommand(" INSERT INTO [dbo].[Employee]([EmpName],[Phone],[Email],[Address]) VALUES(@c1,@c2,@c3,@c4)", conn))
+                            {
+
+                                cmd.Parameters.AddWithValue("@C1", row.Cells[1].Value);
+                                cmd.Parameters.AddWithValue("@C2", row.Cells[2].Value);
+                                cmd.Parameters.AddWithValue("@C3", row.Cells[3].Value);
+                                cmd.Parameters.AddWithValue("@C4", row.Cells[4].Value);
+
+                                cmd.ExecuteNonQuery();
+                                count++;
+                            }
+                            
+                        }                       
+                    }
+                    conn.Close();
+                    MessageBox.Show(count + " Rows inserted.");
+                    dataGridView1.DataSource = null;
+                    dataGridView1.Rows.Clear();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);              
+            }
+            
+        }
+    }
+}
